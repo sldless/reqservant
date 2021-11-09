ead89a2ce025aa1bf6ed9eecf93c28575f22c6d4
sldless <67599596+sldless@users.noreply.github.com>
2021-11-08 22:37:21 -0500

    Update whois.js

--- src/Commands/whois.js
+++ src/Commands/whois.js
@@ -12,57 +12,23 @@ module.exports = {
     }),
   async run(interaction) {
     let user = interaction.options.getUser("member");
-
-    if (!user) {
-      user = interaction.user;
-    }
-
+    user ? user : user = interaction.user;  
     const member = interaction.guild.members.cache.get(user.id);
-
-    const colors = ["#2ECC71", "#E91E63", "#3498DB", "#F1C40F"];
-
     const userEmbed = new MessageEmbed()
       .setColor(
         member.displayHexColor !== "#000000"
           ? member.displayHexColor
-          : colors[Math.floor(Math.random() * colors.length)]
+          : "RANDOM"
       )
-      .setAuthor(
-        `${user.tag} ( ${
-          user.bot ? "is a bot... kinda cringe" : "is an actual person"
-        } )`,
-        user.displayAvatarURL({ dynamic: true })
-      )
+      .setAuthor(`${user.tag} (${user.bot ? "is a bot... kinda cringe" : "is an actual person"})`, user.displayAvatarURL({ dynamic: true }))
       .setThumbnail(user.displayAvatarURL({ dynamic: true, size: 256 }))
-      .addFields(
-        {
-          name: `Join Stats`,
-          value: `> **Joined**: **<t:${Math.floor(
-            member.joinedTimestamp / 1000
-          )}:R>**\n> **Created**: **<t:${Math.floor(
-            user.createdTimestamp / 1000
-          )}:R>**`,
-        },
-        {
-          name: "ID, Nickname, Username, etc...",
-          value: `${
-            member.nickname
-              ? `**Nickname is set to \`${member.nickname}\`**`
-              : "**User has no nickname :/**"
-          }\n> **Snowflake**: ${user.id}\n> **Username**: ${user.username}`,
-        },
-        {
-          name: `Roles (${
-            member.roles.cache.filter((r) => r.id !== "788789142343122945").size
-          })`,
-          value: `${member.roles.cache
-            .filter((r) => r.id !== "788789142343122945")
-            .sorted((r1, r2) => r2.position - r1.position)
-            .map((r) => r)
-            .join(", ")}`,
-        }
-      );
-
+      .addFields({
+        name: `Join Stats`,
+        value: `> **Joined**: **<t:${Math.floor(member.joinedTimestamp / 1000 )}:R>**\n> **Created**: **<t:${Math.floor(user.createdTimestamp / 1000)}:R>**`},
+        {name: "ID, Nickname, Username, etc...", 
+         value: `${member.nickname? `**Nickname is set to \`${member.nickname}\`**`: "**User has no nickname :/**"}\n> **Snowflake**: ${user.id}\n> **Username**: ${user.username}`,},
+        {name: `Roles (${member.roles.cache.filter((r) => r.id !== "788789142343122945").size})`,
+          value: `${member.roles.cache.filter((r) => r.id !== "788789142343122945").sorted((r1, r2) => r2.position - r1.position).map((r) => r).join(", ")}`});
     interaction.reply({ embeds: [userEmbed] });
   },
 };
