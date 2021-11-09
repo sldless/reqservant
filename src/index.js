eaf343c97e5f77a7d921106c0185a946c14c809b
sldless <67599596+sldless@users.noreply.github.com>
2021-11-09 09:42:18 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,19 @@
 const { token } = require("./config.json");
 const { Client, Intents, Collection } = require("discord.js");
-const {
-  loadCommands,
-  loadEvents,
-  loadDatabase,
-} = require("./Utilities/loaders");
+const { loadCommands, loadEvents, loadDatabase, } = require("./Utilities/loaders");
 
 globalThis.log = require("./Utilities/logger");
-
 globalThis.client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MEMBERS,
-  ],
-  presence: {
-    activities: [
-      {
+  intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,Intents.FLAGS.GUILD_MEMBERS],
+  presence: { activities: [{
         name: `Loading...`,
         type: "PLAYING",
-      },
-    ],
-    status: "dnd",
+      },],status: "dnd",
   },
-  allowedMentions: {
-    repliedUser: false,
-  }
+  allowedMentions: { repliedUser: false }
 });
-
 globalThis.commands = new Collection();
 globalThis.events = new Collection();
-
 loadCommands();
 loadEvents();
 loadDatabase();
